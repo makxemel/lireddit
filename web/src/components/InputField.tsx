806fe92f0907e7cca375e5ed2eca2cc96bc53d59
makx <delmst@yandex.ru>
2020-10-17 00:53:26 +0300

    fix input bgcolor

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -24,7 +24,7 @@ export const InputField: React.FC<InputFieldProps> = ({
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <Input {...field} {...props} id={field.name} backgroundColor='grey'/>
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
