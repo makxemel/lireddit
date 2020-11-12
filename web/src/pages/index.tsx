651be86cd43dcf4df56eb6625da1f5b1d1c6f95e
makx <delmst@yandex.ru>
2020-11-12 03:06:57 +0300

    urql-ssr

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,17 @@
+import { withUrqlClient } from "next-urql";
 import { Navbar } from "../components/Navbar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
+const Index = () => {
+    const [{data}] = usePostsQuery();
+    return (
     <>
         <Navbar />
         <div>hello world</div>
+        {!data ? null : data.posts.map(p => <div key={p.id}>{p.title}</div>)}
     </>
-);
+    )
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, {ssr: true})(Index);
